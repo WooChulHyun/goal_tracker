2b9687d7b9b63bf0479f756dc2cf3f195f364c43
HanHyunJin-mis <forinpea121@gmail.com>
2019-03-25 19:29:08 +0900

    feat : add stamp

---
+++ main.js
@@ -0,0 +1,15 @@
+var num = 31;
+$('.test_btn').click(function(){
+    for(i=1; i<=num; i++ ){
+    $('.total_stamp').append('<div class="stamp_container '+i+'">' + '<p class="number">'+ i +'</p>' + '<img src="/sun.png" class="stamp" alt=""></img>'+'</div>');
+    }
+    $('.stamp_container').click(function(){
+        if ( $(this).find('.stamp').hasClass("stamp_click")) {
+            $(this).find('.stamp').removeClass('stamp_click');
+        }else{
+            $(this).find('.stamp').addClass('stamp_click');
+        }
+    });
+    
+});
+
