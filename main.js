9b232c9e144f632b9789f9228675bc4279751129
HanHyunJin-mis <forinpea121@gmail.com>
2019-03-26 10:58:37 +0900

    feat : 190325_merge

--- main.js
+++ main.js
@@ -29,27 +29,11 @@ function closebtn(){
         $(".mdbt").hide();
     });
     
-// 스탬프 구현
-var num = 31;
-$('.test_btn').click(function(){
-    for(i=1; i<=num; i++ ){
-    $('.total_stamp').append('<div class="stamp_container '+i+'">' + '<p class="number">'+ i +'</p>' +
-    '<img class="stamp" src="/sun.png" alt="">' 
-    + '</div>');
-    }
 
-    $('.stamp_container').click(function(){
-        if($(this).find('.stamp').hasClass('stamp_click')){
-            $(this).find('.stamp').removeClass('stamp_click');
-        }else{
-            $(this).find('.stamp').addClass('stamp_click');
-        }
-    });
-});
 
 // 목표 저장
-var count = 0;
 
+var content_dict;
 $(function() {
    $("#btn_sub1").click(function() {
        var storage_key = $("#goals").val();
@@ -72,12 +56,78 @@ $(function() {
        localStorage.setItem(storage_key, form_content);
 
        var content_string_load = localStorage.getItem(storage_key);
-       var content_dict = JSON.parse(content_string_load);
+       content_dict = JSON.parse(content_string_load);
        for (var key in content_dict) {
            if (content_dict[key] === "") {
                delete content_dict[key];
            }
        }
        console.log(content_dict);
+       for(i=1; i<=content_dict.numbs; i++ ){
+            $('.total_stamp').append('<div class="stamp_container '+i+'">' + '<p class="number">'+ i +'</p>' +
+            '<img class="stamp" src="/sun.png" alt="">' 
+            + '</div>');
+            }
+
+        $('.stamp_container').click(function(){
+            if($(this).find('.stamp').hasClass('stamp_click')){
+                $(this).find('.stamp').removeClass('stamp_click');
+            }else{
+                $(this).find('.stamp').addClass('stamp_click');
+            }
+        });
    });
 });
+
+$(".pink").click(function(){
+    $('.grid_sections2').removeClass('darkblue');
+    $('.grid_sections2').addClass('pink');
+    $('.grid_sections2').removeClass('green');
+    $('.grid_sections2').removeClass('purple');
+    $('.grid_sections2').removeClass('blue');
+ });
+ 
+ $(".green").click(function(){
+    $('.grid_sections2').removeClass('darkblue');
+    $('.grid_sections2').removeClass('pink');
+    $('.grid_sections2').addClass('green');
+    $('.grid_sections2').removeClass('purple');
+    $('.grid_sections2').removeClass('blue');
+ });
+ 
+ $(".purple").click(function(){
+    $('.grid_sections2').removeClass('darkblue');
+    $('.grid_sections2').addClass('pink');
+    $('.grid_sections2').removeClass('green');
+    $('.grid_sections2').addClass('purple');
+    $('.grid_sections2').removeClass('blue');
+ });
+ 
+ $(".blue").click(function(){
+    $('.grid_sections2').removeClass('darkblue');
+    $('.grid_sections2').addClass('pink');
+    $('.grid_sections2').removeClass('green');
+    $('.grid_sections2').removeClass('purple');
+    $('.grid_sections2').addClass('blue');
+ });
+
+// console.log(content_dict.numbs);
+
+// 스탬프 구현
+// var num = 31;
+// $('.test_btn').click(function(){
+//     for(i=1; i<=content_dict.numbs; i++ ){
+//         console.log(content_dict.numbs);
+//     $('.total_stamp').append('<div class="stamp_container '+i+'">' + '<p class="number">'+ i +'</p>' +
+//     '<img class="stamp" src="/sun.png" alt="">' 
+//     + '</div>');
+//     }
+
+//     $('.stamp_container').click(function(){
+//         if($(this).find('.stamp').hasClass('stamp_click')){
+//             $(this).find('.stamp').removeClass('stamp_click');
+//         }else{
+//             $(this).find('.stamp').addClass('stamp_click');
+//         }
+//     });
+// });
